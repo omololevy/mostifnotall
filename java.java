32128ac5d896cb9d2ebf8ad984f6719f43cf29a0
omololevy <cotechlevy@gmail.com>
2022-03-19 12:17:49 +0300

    Create a hello world and addition function

---
+++ java.java
@@ -0,0 +1,14 @@
+public class java {
+    /* This is my first java program.
+     * This will print 'Hello World' as the output
+     */
+    public static int add (int num1,int num2){
+        return num2 * num1;
+    }
+ 
+    public static void main(String []args) {
+       System.out.println("Hello World"); // prints Hello World
+       System.out.println("The product of the two integers is " + add(30, 15));
+    }
+
+ }
