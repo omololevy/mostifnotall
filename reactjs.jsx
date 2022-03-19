b6dfa02121bdceeb6b71c5457bc825a268d2d516
omololevy <cotechlevy@gmail.com>
2022-03-20 01:27:24 +0300

    react js prog file

---
+++ reactjs.jsx
@@ -0,0 +1,13 @@
+let levy = {
+    age: 24,
+    second_name: 'Omolo'
+};
+console.log(levy.age);
+if(levy.age < 28)
+{
+    console.log("You can't be a president!");
+
+}
+else{
+    console.log("Proceed to the campaigns. Good Luck!");
+}
